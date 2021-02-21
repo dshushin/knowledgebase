36e8193312940e63f8fc9d35e260081f931e4a4f
Denis <denis@shushin.io>
2021-02-21 20:09:33 +0300

    frontend added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users', include('users.urls')),
-    path('api/kb', include('knowledgebase.urls'))
+    # path('api/users', include('users.urls')),
+    # path('api/kb', include('knowledgebase.urls'))
 ]
